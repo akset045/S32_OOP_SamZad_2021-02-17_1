a8826cee54fa6a406f89a3847aee73d5ef51dc5b
Akset045 <Akset045@gmail.com>
2021-03-02 01:12:33 +0300

    Add project files.

---
+++ SamZad_21-02-17_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamZad_21_02_17_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введите число: ");
+            Double A = Double.Parse(Console.ReadLine());
+
+            Console.Write("Послдовтельность: " + (A-1) + " ; " + (A) + " ; " + (A +1));
+            Console.ReadKey();
+        }
+    }
+}
